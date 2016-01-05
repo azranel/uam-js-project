381d314201e546cad4f85578c50c834c02b226fa
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2016-01-05 12:11:55 +0100

    Change extension to jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,99 @@
+require('normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+import ProductStore from '../stores/ProductStore';
+import ProductActions from '../actions/ProductActions';
+
+import Product from './products/product';
+
+class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        {
+          "id": 1,
+          "name": "Margherita",
+          "ingredients": [
+            "Sos",
+            "Ser"
+          ],
+          "price": 14.9
+        },
+        {
+          "id": 2,
+          "name": "Funghi ",
+          "ingredients": [
+            "Sos",
+            "Ser",
+            "Pieczarki"
+          ],
+          "price": 16.9
+        },
+        {
+          "id": 3,
+          "name": "Vesuvio",
+          "ingredients": [
+            "Sos",
+            "Ser",
+            "Szynka"
+          ],
+          "price": 17.9
+        },
+        {
+          "id": 4,
+          "name": "Salami",
+          "ingredients": [
+            "Sos",
+            "Ser",
+            "Salami"
+          ],
+          "price": 17.9
+        }
+      ]
+    };
+    ProductActions.getProducts();
+    this._onProductChange = this._onProductChange.bind(this);
+  }
+
+  componentDidMount() {
+    ProductStore.listen(this._onProductChange);
+  }
+
+  componentWillUnmount() {
+    ProductStore.unlisten(this._onProductChange);
+  }
+
+  _onProductChange(store) {
+    this.setState({ products: store.products });
+  }
+
+  products() {
+    const productRows = this.state.products.map(product => <Product key={product.id} product={product} />);
+    return (
+      <div className="products">
+        {productRows}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="index">
+        <header className='text-center'>
+          <h2>HELLO WORLD!</h2>
+        </header>
+        <div className='row'>
+          {this.products()}
+          <div className="basket"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
